0a40872c8166736bbfc4a3645e8274eb4dd85215
Luke Sweeney <llsween95@gmail.com>
2020-04-27 20:38:46 -0400

    api issues

--- find-n-dine/src/App.js
+++ find-n-dine/src/App.js
@@ -4,10 +4,35 @@ import Survey from './Survey'
 import Results from './Results'
 import Footer from './Footer'
 import { Route, Link, Switch } from 'react-router-dom'
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      businesses: []
+    }
+  }
+
+
+
+
+  async componentDidMount() {
+
+    const apiCall = await axios({
+      baseURL: 'https://api.yelp.com/v3/businesses/',
+      headers: {
+        Authorization: 'Bearer VH55OzljuVjIfTgkNbrFZSKpqrDzuldnxMc6DooDLTrVhl_swACA-XW_qqzlkblwB2LgJ_hKKpthcNfoH5_UiJ1wMvHEZJNI0CFrk75YBKOaHrWi7DcOr_KcmLikXnYx',
+      }
+    })
+    console.log('hi')
+
+  }
+
+
   render() {
+    //console.log(process.env.REACT_APP_FAVLIQUOR)
     return (
       <div className="App">
         <Header />
